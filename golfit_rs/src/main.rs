9e0abc568a99bc7b6c685ba77098bfe56bd37dad
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-08-23 15:46:51 -0600

    Buttons and code refactor

--- golfit_rs/src/main.rs
+++ golfit_rs/src/main.rs
@@ -1,16 +1,34 @@
 use raylib::prelude::*;
 mod scenes;
 mod constants;
-use scenes::home::draw_home_menu;
+use scenes::home::{
+    draw_checkerboard_tiles,
+    draw_menu_card_background,
+    draw_menu_action_buttons,
+    draw_menu_title
+};
 use constants::game_details::*;
+
+struct Level {
+    level: i32,
+}
+
+impl Level {
+    fn new(level: i32) -> Self {
+        Level { level }
+    }
+
+    fn set_last_played_level(&mut self) {
+        self.level = 1; // For simplicity, always set to level 1
+    }
+}
 
 enum Scene {
     Home,
     Levels,
     Game,
+    Options,
 }
-
-pub const GAME_NAME: &str = "GolfRs";
 
 fn main() {
     let (mut handler, thread) = raylib::init().size(10, 10).title(GAME_NAME).build();
@@ -23,31 +41,34 @@ fn main() {
         handler.set_window_size(window_width, window_height);
 
         // Try to load a custom font, fall back to default font if not found
-        
         let data: &[u8] = include_bytes!("./resources/chewy.ttf"); // path is compile-time
         let custom_font = handler.load_font_from_memory(&thread, ".ttf", data, 64, None)
             .expect("Failed to load embedded font");
 
         let scene = Scene::Home; // The starting scene, during dev it can change, in prod it should be Scene::Home.
+        let level = Level::new(1); // Start at level 1
 
         while !handler.window_should_close() {
-            let smallest_orientation = window_width.min(window_height); // Get the smallest window orientation
-
             let mut drawing = handler.begin_drawing(&thread);
 
             drawing.clear_background(Color::BLACK);
 
             match scene {
                 Scene::Home => {
-                    draw_home_menu(
-                        &mut drawing,
-                        window_width,
-                        window_height,
-                        smallest_orientation,
-                        &custom_font,
-                    );
+                    // Draw the background green tiles
+                    draw_checkerboard_tiles(&mut drawing, window_width, window_height);
+
+                    // Draw the menu card background
+                    let card_menu = draw_menu_card_background(&mut drawing, window_width as f32, window_height as f32);
+
+                    // Draw the action buttons
+                    let button_section = draw_menu_action_buttons(&mut drawing, card_menu, &custom_font);
+
+                    // Draw the title
+                    draw_menu_title(&mut drawing, card_menu, button_section, &custom_font);
                 }
                 Scene::Levels => {}
+                Scene::Options => {}
                 Scene::Game => {}
             }
         }
--- golfit_rs/src/scenes/home.rs
+++ golfit_rs/src/scenes/home.rs
@@ -1,14 +1,32 @@
 use raylib::prelude::*;
-use crate::constants::game_details::*;
+use raylib::ffi;
+use std::ffi::CString;
+use crate::{constants::game_details::*};
 
-fn draw_checkerboard_tiles(
+
+fn measure_text_ex_safe(font: &Font, text: &str, font_size: f32, spacing: f32) -> Vector2 {
+    let c_text = CString::new(text).unwrap();
+    unsafe {
+        let ffi_vector2 = ffi::MeasureTextEx(**font, c_text.as_ptr(), font_size, spacing);
+        Vector2::new(ffi_vector2.x, ffi_vector2.y)
+    }
+}
+
+/// Draws a checkerboard pattern of tiles to fill the entire window.
+///
+/// # Arguments
+/// 
+/// * `drawing` - The Raylib drawing handle.
+/// * `window_width` - The width of the window.
+/// * `window_height` - The height of the window.
+/// * `smallest_orientation` - The smallest orientation of the window (width or height).
+pub fn draw_checkerboard_tiles(
     drawing: &mut RaylibDrawHandle,
     window_width: i32,
-    window_height: i32,
-    smallest_orientation: i32,
+    window_height: i32
 ) {
     let tiles_count = 15;
-    let tile_size = smallest_orientation / tiles_count;
+    let tile_size = window_height / tiles_count;
     let horizontal_tiles = window_width / tile_size;
     let vertical_tiles = window_height / tile_size;
 
@@ -26,97 +44,179 @@ fn draw_checkerboard_tiles(
     }
 }
 
-fn draw_menu_background(
+/// Draws the menu card background.
+/// 
+/// # Arguments
+/// 
+/// * `drawing` - The Raylib drawing handle.
+/// * `window_width` - The width of the window.
+/// * `window_height` - The height of the window.
+/// 
+/// # Returns
+///
+/// A vector containing the x, y, width, and height of the menu card background.
+///
+/// # Examples
+/// 
+/// ```
+/// let menu_card_background = draw_menu_card_background(drawing, window_width, window_height);
+/// ```
+pub fn draw_menu_card_background(
     drawing: &mut RaylibDrawHandle,
-    window_width: i32,
-    window_height: i32,
-) -> (i32, i32, i32, i32) {
+    window_width: f32,
+    window_height: f32,
+) -> Vector4 {
     // Draw a 80% tall 60% wide dark green rectangle in the middle
-    let rect_width = (window_width as f32 * 0.6) as i32;
-    let rect_height = (window_height as f32 * 0.8) as i32;
+    let card_width = (window_width * 0.6) as i32;
+    let card_height = (window_height * 0.8) as i32;
 
-    let rect_x = (window_width - rect_width) / 2;
-    let rect_y = (window_height - rect_height) / 2;
+    let card_x = (window_width as i32 - card_width) / 2;
+    let card_y = (window_height as i32 - card_height) / 2;
 
-    drawing.draw_rectangle(rect_x, rect_y, rect_width, rect_height, Color::DARKGREEN);
+    drawing.draw_rectangle(card_x, card_y, card_width, card_height, Color::DARKGREEN);
 
-    // Draw a 2-lined border, golden color for the rectangle, 20px
-    // Top lines
-    drawing.draw_rectangle(rect_x, rect_y, rect_width, 15, Color::GOLD);
-    drawing.draw_rectangle(rect_x + 30, rect_y + 30, rect_width - 60, 15, Color::GOLD);
-    // Bottom lines
-    drawing.draw_rectangle(
-        rect_x,
-        rect_y + rect_height - 15,
-        rect_width,
-        15,
-        Color::GOLD,
-    );
-    drawing.draw_rectangle(
-        rect_x + 30,
-        rect_y + rect_height - 45,
-        rect_width - 60,
-        15,
-        Color::GOLD,
-    );
-    // Left lines
-    drawing.draw_rectangle(rect_x, rect_y, 15, rect_height, Color::GOLD);
-    drawing.draw_rectangle(rect_x + 30, rect_y + 30, 15, rect_height - 60, Color::GOLD);
-    // Right lines
-    drawing.draw_rectangle(
-        rect_x + rect_width - 15,
-        rect_y,
-        15,
-        rect_height,
-        Color::GOLD,
-    );
-    drawing.draw_rectangle(
-        rect_x + rect_width - 45,
-        rect_y + 30,
-        15,
-        rect_height - 60,
-        Color::GOLD,
-    );
+    // Draw a 2-lined border, golden color for the rectangle. Top, bottom, right, left.
+    drawing.draw_rectangle(card_x, card_y, card_width, 15, Color::GOLD);
+    drawing.draw_rectangle(card_x + 30, card_y + 30, card_width - 60, 15, Color::GOLD);
 
-    (rect_x, rect_y, rect_width, rect_height)
+    drawing.draw_rectangle(card_x, card_y + card_height - 15, card_width, 15, Color::GOLD);
+    drawing.draw_rectangle(card_x + 30, card_y + card_height - 45, card_width - 60, 15, Color::GOLD);
+
+    drawing.draw_rectangle(card_x, card_y, 15, card_height, Color::GOLD);
+    drawing.draw_rectangle(card_x + 30, card_y + 30, 15, card_height - 60, Color::GOLD);
+
+    drawing.draw_rectangle(card_x + card_width - 15, card_y, 15, card_height, Color::GOLD);
+    drawing.draw_rectangle(card_x + card_width - 45, card_y + 30, 15, card_height - 60, Color::GOLD);
+
+    Vector4::new(card_x as f32, card_y as f32, card_width as f32, card_height as f32)
 }
 
-fn draw_menu_title(
+pub fn draw_change_scene_button(
     drawing: &mut RaylibDrawHandle,
-    window_width: i32,
-    rect_y: i32,
-    rect_height: i32,
+    rectangle: Vector4,
+    label: &str,
+    custom_font: &Font
+) {
+    let x = rectangle.x as i32;
+    let y = rectangle.y as i32;
+    let width = rectangle.z as i32;
+    let height = rectangle.w as i32;
+
+    // Draw button background
+    drawing.draw_rectangle(x, y, width, height, Color::DARKGREEN);
+
+    // Draw button borders top, bottom, left, right
+    drawing.draw_rectangle(x, y, width, 5, Color::GOLD);
+    drawing.draw_rectangle(x, y + height - 5, width, 5, Color::GOLD);
+    drawing.draw_rectangle(x, y, 5, height, Color::GOLD);
+    drawing.draw_rectangle(x + width - 5, y, 5, height, Color::GOLD);
+
+    let font_size = 60;
+    let gap = 1.0;
+
+    let button_center_x = x as f32 + (width as f32 / 2.0);
+    let button_center_y = y as f32 + (height as f32 / 2.0);
+
+    // Draw button label
+    drawing.draw_text_ex(
+        custom_font,
+        label,
+        Vector2::new(button_center_x - (measure_text_ex_safe(custom_font, label, font_size as f32, gap).x / 2.0), button_center_y - (font_size as f32 / 2.0)),
+        font_size as f32,
+        gap,
+        Color::GOLD,
+    );
+}
+
+/// Draws the action buttons for the menu.
+/// 
+/// # Arguments
+/// * `drawing` - The Raylib drawing handle.
+/// * `menu_card` - A Vector4 representing the x, y, width, and height of the menu card.
+/// * `custom_font` - The custom font to use for the buttons
+/// * `handler` - The Raylib input handler
+/// * `scene` - The current game scene
+/// * `level` - The current game level
+pub fn draw_menu_action_buttons(
+    drawing: &mut RaylibDrawHandle,
+    card_menu: Vector4,
+    custom_font: &Font
+) -> Vector4 {
+    let menu_card_width = card_menu.z;
+    let menu_card_height = card_menu.w;
+    
+    let button_width = menu_card_width / 3.0;
+    let button_height = 90.0;
+
+    let bottom_margin = 20.0;
+
+    // Calculate the center positions
+    // X axis is the start position of the menu card + its width in the middle, minus half the button width
+    // Y axis is the start position of the menu card + its height in the middle, minus half the button height and the border of the card (45.0)
+    let center_x = (card_menu.x + (menu_card_width / 2.0)) - (button_width / 2.0);
+    let center_y = card_menu.y + menu_card_height - button_height - 45.0 - bottom_margin;
+
+    let options_button = Vector4::new(center_x, center_y, button_width, button_height);
+    let levels_button = Vector4::new(center_x, center_y - button_height - bottom_margin, button_width, button_height);
+    let continue_button = Vector4::new(center_x, center_y - 2.0 * button_height - 2.0 * bottom_margin, button_width, button_height);
+
+    draw_change_scene_button(drawing, continue_button, "Continue", custom_font);
+    draw_change_scene_button(drawing, levels_button, "Levels", custom_font);
+    draw_change_scene_button(drawing, options_button, "Options", custom_font);
+
+    Vector4::new(card_menu.x, center_y - 2.0 * button_height - 2.0 * bottom_margin, menu_card_width, 3.0 * button_height + 2.0 * bottom_margin)
+}
+
+/// Draws the name of the game with custom font in card menu above the buttons.
+/// 
+/// # Arguments
+/// * `drawing` - The Raylib drawing handle.
+/// * `rect_x` - The x position of the menu card.
+/// * `rect_y` - The y position of the menu card.
+/// * `rect_width` - The width of the menu card.
+/// * `rect_height` - The height of the menu card.
+/// * `button_section_x` - The x position of the button section.
+/// * `button_section_y` - The y position of the button section.
+/// * `button_section_width` - The width of the button section.
+/// * `button_section_height` - The height of the button section.
+/// * `custom_font` - The custom font to use for the title.
+/// 
+/// # Returns
+/// A tuple containing the x, y, width, and height of the title text.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let (x, y, width, height) = draw_menu_title(drawing, rect_x, rect_y, rect_width, rect_height, button_section_x, button_section_y, button_section_width, button_section_height, custom_font);
+/// ```
+pub fn draw_menu_title(
+    drawing: &mut RaylibDrawHandle,
+    card_menu: Vector4,
+    button_section: Vector4,
     custom_font: &Font,
 ) {
     let title = GAME_NAME;
     let font_size = 150;
+    let gap = 1.0;
 
-    // For custom font, we'll use a simple approach since measure_text_ex doesn't exist
-    // We'll estimate the width or just center it manually
-    let estimated_text_width = title.len() as i32 * (font_size / 3); // Rough estimation
-    let title_x = (window_width - estimated_text_width) / 2;
-    let title_y = rect_y + rect_height / 2 - font_size / 2;
+    let rect_x = card_menu.x as i32;
+    let rect_y = card_menu.y as i32;
+    let rect_width = card_menu.z as i32;
+    let rect_height = card_menu.w as i32;
+
+    let button_section_y = button_section.y as i32;
+
+    let text_width = measure_text_ex_safe(custom_font, title, font_size as f32, gap).x as i32;
+
+    let title_x = (rect_x + rect_width / 2) - (text_width / 2);
+    let title_y = (rect_y + rect_height / 2 - font_size / 2) - (button_section_y - rect_y) / 2;
+
     drawing.draw_text_ex(
         custom_font,
         title,
         Vector2::new(title_x as f32, title_y as f32),
         font_size as f32,
-        1.0,
+        gap,
         Color::GOLD,
     );
-}
-
-pub fn draw_home_menu(
-    drawing: &mut RaylibDrawHandle,
-    window_width: i32,
-    window_height: i32,
-    smallest_orientation: i32,
-    custom_font: &Font,
-) {
-    draw_checkerboard_tiles(drawing, window_width, window_height, smallest_orientation);
-
-    let (rect_x, rect_y, rect_width, rect_height) =
-        draw_menu_background(drawing, window_width, window_height);
-
-    draw_menu_title(drawing, window_width, rect_y, rect_height, custom_font);
 }
