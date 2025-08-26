b2964ab2808ffd9e5bda7ee8dee33babfffec433
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-08-26 16:51:19 -0600

    Restart pacman

--- pacman_rs/src/main.rs
+++ pacman_rs/src/main.rs
@@ -1,11 +1,3 @@
-mod types;
-mod player;
-mod ghost;
-mod rendering;
-mod game_map;
-mod game;
-
-use game::Game;
 use raylib::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,26 +12,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     rl.set_target_fps(60);
     rl.toggle_fullscreen();
     
-    // Initialize game
-    let mut game = Game::new();
-    
-    // Load resources
-    if let Err(e) = game.load_resources(&mut rl, &thread) {
-        eprintln!("Failed to load game resources: {}", e);
-        // Continue anyway - the game will use fallback rendering
-    }
-    
     // Game loop
     while !rl.window_should_close() {
-        // Handle input
-        game.handle_input(&rl);
-        
-        // Update game state
-        game.update();
-        
-        // Draw everything
         let mut d = rl.begin_drawing(&thread);
-        game.draw(&mut d);
+
+        d.clear_background(Color::BLACK);
+
     }
     
     Ok(())
