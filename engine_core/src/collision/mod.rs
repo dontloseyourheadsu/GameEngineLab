a1513202f8c699f639c7ae7eb8a552d4bee1cb5a
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-08-27 18:30:20 -0600

    Physic object

--- engine_core/src/collision/mod.rs
+++ engine_core/src/collision/mod.rs
@@ -1,5 +1,9 @@
+pub mod circle_collision_mesh;
 pub mod collision_mesh;
 pub mod evaluator;
+pub mod rectangle_collision_mesh;
 
+pub use circle_collision_mesh::CircleCollisionMesh;
 pub use collision_mesh::CollisionMesh;
-pub use evaluator::resolve_all_collisions;
+pub use evaluator::{resolve_all_collisions, resolve_collision, check_collision};
+pub use rectangle_collision_mesh::RectangleCollisionMesh;
