c9b4cfdce442d1cfc67ac65cd6397ba0b2c43ed1
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-08-29 13:07:54 -0600

    Solid body

--- engine_core/src/lib.rs
+++ engine_core/src/lib.rs
@@ -3,9 +3,11 @@ pub mod drawing;
 pub mod rendering;
 pub mod tilemap;
 pub mod utilities;
+pub mod physics;
 
 pub use background::*;
 pub use drawing::*;
 pub use rendering::*;
 pub use tilemap::*;
+pub use physics::*;
 pub use utilities::*;
