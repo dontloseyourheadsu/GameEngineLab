af1c13fb56287b5468e796fa088f6c0f23252b36
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-08-29 12:08:07 -0600

    Replace physics with rapier lib

--- engine_core/src/collision/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod circle_collision_mesh;
-pub mod collision_mesh;
-pub mod evaluator;
-pub mod rectangle_collision_mesh;
-
-pub use circle_collision_mesh::CircleCollisionMesh;
-pub use collision_mesh::CollisionMesh;
-pub use evaluator::{resolve_all_collisions, resolve_collision, check_collision};
-pub use rectangle_collision_mesh::RectangleCollisionMesh;
--- engine_core/src/physics/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod box_object;
-pub mod physics_object;
-pub mod verlet_point;
-
-pub use box_object::BoxObject;
-pub use physics_object::PhysicsObject;
-pub use verlet_point::VerletPoint;
