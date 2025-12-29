4f3c9ab52f26e97c4b4c0b5656fa872c0949105e
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-28 21:52:20 -0600

    map 2d resources and models

--- engine_core/src/maps/map_2d_model.rs
+++ engine_core/src/maps/map_2d_model.rs
@@ -1,8 +1,10 @@
 use bevy::ecs::component::Component;
+use std::collections::HashMap;
 
 #[derive(Component)]
 pub struct Map2DModel {
     pub width: usize,
     pub height: usize,
     pub data: Vec<String>,
+    pub symbols: HashMap<String, String>, // Mapping of symbols to their meanings
 }
