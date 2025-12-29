8b0c727b93eb83d6cc7614be7b9526e1747e5787
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-29 11:15:35 -0600

    Add texture fields to map

--- engine_core/src/maps/map_2d_model.rs
+++ engine_core/src/maps/map_2d_model.rs
@@ -3,8 +3,10 @@ use std::collections::HashMap;
 
 #[derive(Component)]
 pub struct Map2DModel {
-    pub width: usize,
-    pub height: usize,
+    pub tile_width: usize,
+    pub tile_height: usize,
     pub data: Vec<String>,
     pub symbols: HashMap<String, String>, // Mapping of symbols to their meanings
+    pub textures_path: String,            // Path to the textures directory
+    pub textures: HashMap<String, String>, // Mapping of symbols to texture file names
 }
--- engine_core/src/maps/map_json.rs
+++ engine_core/src/maps/map_json.rs
@@ -1,10 +1,14 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 
 use crate::maps::tile_size::TileSize;
 
 #[derive(Deserialize)]
 pub struct MapJson {
-    pub symbols: std::collections::HashMap<String, String>,
+    pub symbols: HashMap<String, String>,
+    pub textures_path: String,
+    pub textures: HashMap<String, String>,
     pub tile_size: TileSize,
     pub map_data: String,
 }
