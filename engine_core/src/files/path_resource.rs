3475fe35b80aa24519df4a0e8d80fe07c277872f
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-29 19:35:28 -0600

    improved resource reading

--- engine_core/src/files/path_resource.rs
+++ engine_core/src/files/path_resource.rs
@@ -1,4 +1,4 @@
-use bevy::ecs::resource::Resource;
+use bevy::ecs::system::Resource;
 
 #[derive(Resource)]
 pub struct PathResource(pub String);
--- engine_core/src/maps/map_2d_json_serializer.rs
+++ engine_core/src/maps/map_2d_json_serializer.rs
@@ -7,35 +7,34 @@ use crate::{
     maps::{map_2d_model::Map2DModel, map_2d_resource::Map2DResource, map_json::MapJson},
 };
 
-pub fn map_2d_json_serializer_system(mut commands: Commands, path: Option<Res<PathResource>>) {
-    if let Some(path) = path {
-        let content = fs::read_to_string(&path.0).expect("Failed to read map file");
-        let map_json: MapJson =
-            serde_json::from_str(&content).expect("Failed to parse JSON map file");
+pub fn map_2d_json_serializer_system(
+    mut commands: Commands,
+    path_resource: Res<PathResource>,
+) {
+    let content = fs::read_to_string(&path_resource.0).expect("Failed to read map file");
+    let map_json: MapJson =
+        serde_json::from_str(&content).expect("Failed to parse JSON map file");
 
-        let width = map_json.tile_size.width;
-        let height = map_json.tile_size.height;
-        let data: Vec<String> = map_json
-            .map_data
-            .lines()
-            .map(|line| line.to_string())
-            .collect();
+    let width = map_json.tile_size.width;
+    let height = map_json.tile_size.height;
+    let data: Vec<String> = map_json
+        .map_data
+        .lines()
+        .map(|line| line.to_string())
+        .collect();
 
-        let symbols: HashMap<String, String> = map_json.symbols; // Parse symbols mapping
-        let textures_path = map_json.textures_path; // Parse textures path
-        let textures = map_json.textures; // Parse textures mapping
+    let symbols: HashMap<String, String> = map_json.symbols; // Parse symbols mapping
+    let textures_path = map_json.textures_path; // Parse textures path
+    let textures = map_json.textures; // Parse textures mapping
 
-        let map = Map2DModel {
-            tile_width: width,
-            tile_height: height,
-            data,
-            symbols,
-            textures_path,
-            textures,
-        };
+    let map = Map2DModel {
+        tile_width: width,
+        tile_height: height,
+        data,
+        symbols,
+        textures_path,
+        textures,
+    };
 
-        commands.insert_resource(Map2DResource { map });
-    } else {
-        eprintln!("PathResource is missing. Skipping map_2d_json_serializer_system.");
-    }
+    commands.insert_resource(Map2DResource { map });
 }
