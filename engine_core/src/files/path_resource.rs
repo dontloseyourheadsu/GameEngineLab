b954cd66fff21bc2aeb0f93d91932d6338ec2189
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-31 17:23:59 -0600

    Delete bevy related code

--- engine_core/src/files/path_resource.rs
+++
@@ -1,4 +0,0 @@
-use bevy::ecs::system::Resource;
-
-#[derive(Resource)]
-pub struct PathResource(pub String);
--- engine_core/src/maps/map_2d_resource.rs
+++
@@ -1,8 +0,0 @@
-use bevy::ecs::system::Resource;
-
-use crate::maps::map_2d_model::Map2DModel;
-
-#[derive(Resource)]
-pub struct Map2DResource {
-    pub map: Map2DModel,
-}
