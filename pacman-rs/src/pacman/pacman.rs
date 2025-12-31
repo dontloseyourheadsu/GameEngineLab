204753d3fef7b72927a22637ed25c86f13b4ed80
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-31 17:24:54 -0600

    pacman update controller

---
+++ pacman-rs/src/pacman/pacman.rs
@@ -0,0 +1,8 @@
+use engine_core::character::character_2d::Character2D;
+use raylib::math::Vector2;
+
+pub struct Pacman {
+    pub character: Character2D,
+    pub current_direction: Vector2,
+    pub desired_direction: Vector2,
+}
