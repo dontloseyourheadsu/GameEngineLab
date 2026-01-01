59d1891c0755d0136ff8c9d47ae0f20e21d131c7
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-31 18:24:46 -0600

    recreate a better map

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -3,5 +3,5 @@ mod serializers;
 use crate::serializers::map_txt_to_json_string_serializer::map_txt_to_json_string_serializer;
 
 fn main() {
-    map_txt_to_json_string_serializer("data/map.txt", "tools/map.json");
+    map_txt_to_json_string_serializer("data/map.txt", "data/map.json");
 }
