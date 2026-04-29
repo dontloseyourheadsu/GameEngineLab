9ac3251a98f66486de955598f128863d2b8e285b
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-04-28 23:55:50 -0600

    Rigid body phisics

---
+++ GameEngineLab.Core/Features/Physics/Systems/MovementSystem.cs
@@ -0,0 +1,25 @@
+using GameEngineLab.Core.Features.Ecs.Resources;
+using GameEngineLab.Core.Features.Ecs.Systems;
+using GameEngineLab.Core.Features.Physics.Components;
+
+namespace GameEngineLab.Core.Features.Physics.Systems;
+
+public sealed class MovementSystem : IGameSystem
+{
+    public int Order => 10;
+
+    public void Update(World world, FrameContext frameContext)
+    {
+        foreach (var entityId in world.GetEntitiesWith<TransformComponent, VelocityComponent>())
+        {
+            world.TryGetComponent<TransformComponent>(entityId, out var transform);
+            world.TryGetComponent<VelocityComponent>(entityId, out var velocity);
+
+            transform.Position += velocity.Value * frameContext.DeltaSeconds;
+            
+            world.SetComponent(entityId, transform);
+        }
+    }
+
+    public void Draw(World world, FrameContext frameContext) { }
+}
---
+++ GameEngineLab.Core/Features/Physics/Systems/PhysicsFrictionSystem.cs
@@ -0,0 +1,36 @@
+using GameEngineLab.Core.Features.Ecs.Resources;
+using GameEngineLab.Core.Features.Ecs.Systems;
+using GameEngineLab.Core.Features.Physics.Components;
+using Microsoft.Xna.Framework;
+
+namespace GameEngineLab.Core.Features.Physics.Systems;
+
+public sealed class PhysicsFrictionSystem : IGameSystem
+{
+    public int Order => 30;
+
+    private const float StopThreshold = 5f;
+
+    public void Update(World world, FrameContext frameContext)
+    {
+        foreach (var entityId in world.GetEntitiesWith<RigidBodyComponent, VelocityComponent>())
+        {
+            world.TryGetComponent<RigidBodyComponent>(entityId, out var body);
+            world.TryGetComponent<VelocityComponent>(entityId, out var velocity);
+
+            if (velocity.Value.LengthSquared() > 0)
+            {
+                velocity.Value *= body.Friction;
+
+                if (velocity.Value.Length() < StopThreshold)
+                {
+                    velocity.Value = Vector2.Zero;
+                }
+
+                world.SetComponent(entityId, velocity);
+            }
+        }
+    }
+
+    public void Draw(World world, FrameContext frameContext) { }
+}
