2eaa07cc7256e503afc76900f699f2c946ef9af4
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-04-30 17:03:48 -0600

    Delete tools and playground

--- tools/src/main.rs
+++
@@ -1,7 +0,0 @@
-mod serializers;
-
-use crate::serializers::map_txt_to_json_string_serializer::map_txt_to_json_string_serializer;
-
-fn main() {
-    map_txt_to_json_string_serializer("data/map.txt", "data/map.json");
-}
